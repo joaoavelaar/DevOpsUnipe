d9387555b80775fbe258c6ac9c900ad83e90eca4
Joao <joaoavelar.unipe@gmail.com>
2025-11-29 09:04:20 -0300

    Commit inicial

---
+++ src/main/java/br/unipe/devops/DevOpsUnipe/DevOpsUnipeApplication.java
@@ -0,0 +1,13 @@
+package br.unipe.devops.DevOpsUnipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsUnipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsUnipeApplication.class, args);
+	}
+
+}
